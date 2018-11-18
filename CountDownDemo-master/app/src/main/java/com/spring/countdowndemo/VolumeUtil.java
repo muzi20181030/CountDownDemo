732fb627ca5ca1701691fd962e45349b52ac5f55
muzi20181030 <44600480+muzi20181030@users.noreply.github.com>
2018-11-18 18:27:13 +0800

    倒计时器

---
+++ CountDownDemo-master/app/src/main/java/com/spring/countdowndemo/VolumeUtil.java
@@ -0,0 +1,30 @@
+package com.spring.countdowndemo;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+//声音播放
+
+
+public class VolumeUtil {
+
+
+    private MediaPlayer mediaPlayer;
+
+    public void playMusic(Context context,int music) {
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+        }
+        mediaPlayer = MediaPlayer.create(context, music);
+        mediaPlayer.start();
+        mediaPlayer.setVolume(1f, 1f);
+    }
+
+    public void releaseMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
